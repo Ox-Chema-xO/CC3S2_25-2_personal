8f3bb1b8a4db1f5820e99b96e3050e217477f74f
Chema <manuelchema999@gmail.com>
2025-09-08 22:49:38 -0500

    feat: se agrega seccion1

--- PE-CC3S2/seccion1_cli_automatizacion/scripts/syscheck.sh
+++ PE-CC3S2/seccion1_cli_automatizacion/scripts/syscheck.sh
@@ -6,25 +6,28 @@ mkdir -p reports
 
 # TODO: HTTP — guarda headers y explica código en 2–3 líneas al final del archivo
 {
-  echo "== curl -I example.com =="
-  curl -Is https://example.com | sed '/^\r$/d'
+  echo "== curl -I google.com =="
+  curl -Is https://google.com | sed '/^\r$/d'
   echo
-  echo "Explicación (editar): Código HTTP X significa ..."
+  echo "Explicación: Código HTTP 301 significa que la URL solicitada ha sido movida permanentemente a una nueva ubicación que es indica en location."
+  echo "Los headers muestran información del servidor como content-type, content-security-policy-report-only, date."
 } > reports/http.txt
 
 # TODO: DNS — muestra A/AAAA/MX y comenta TTL
 {
-  echo "== A ==";    dig A example.com +noall +answer
-  echo "== AAAA =="; dig AAAA example.com +noall +answer
-  echo "== MX ==";   dig MX example.com +noall +answer
+  echo "== A ==";    dig A google.com +noall +answer
+  echo "== AAAA =="; dig AAAA google.com +noall +answer
+  echo "== MX ==";   dig MX google.com +noall +answer
   echo
-  echo "Nota (editar): TTL alto vs bajo impacta en ..."
+  echo "Los registros A y AAAA proporcionan IPs para la resolución de nombres, permitiendo la conectividad tanto en IPv4 como en IPv6."
+  echo "Un TTL bajo aumenta consultas DNS, permite actualizaciones rápidas, pero puede aumentar la carga en los servidores DNS."
+  echo "Un TTL alto reduce la frecuencia de consultas a los servidores DNS, pero los cambios en los registros pueden tardar más en propagarse."
 } > reports/dns.txt
 
 # TODO: TLS — registra versión TLS
 {
   echo "== TLS via curl -Iv =="
-  curl -Iv https://example.com 2>&1 | sed -n '1,20p'
+  curl -Iv https://google.com 2>&1 | sed -n '1,20p'
 } > reports/tls.txt
 
 # TODO: Puertos locales — lista y comenta riesgos
@@ -32,7 +35,7 @@ mkdir -p reports
   echo "== ss -tuln =="
   ss -tuln || true
   echo
-  echo "Riesgos (editar): Puertos abiertos innecesarios pueden ..."
+  echo "Riesgos: Puertos abiertos innecesarios aumentan el rango de ataques como malware, accesos no autorizados o ataques DoS."
 } > reports/sockets.txt
 
 echo "Reportes generados en ./reports"
